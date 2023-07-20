6941c239e72aac06286090248cc57b57b2741cd0
gulser <glserozkan@gmail.com>
2023-07-20 10:06:27 +0200

    header file trial

--- include/minishell.h
+++ include/minishell.h
@@ -4,3 +4,46 @@
 
 # include "../libft/libft.h"
 
+typedef enum s_tokens
+{
+    PIPE = 1,
+    GREAT,
+    GREAT_GREAT,
+    LESS,
+    LESS_LESS,
+}   t_tokens;
+
+typedef struct s_token
+{
+    char            *content;
+    int             token_id;
+    struct s_token  *next;
+}   t_token;
+
+
+typedef struct s_tools
+{
+    char **env;
+    struct s_env_var *env_var;
+    struct s_command *command;
+}t_tool;
+
+
+typedef struct s_command
+{
+    char **args;
+
+
+
+
+}t_command;
+
+BUILTINS
+
+typedef struct s_env_var
+{
+    struct s_env    *next;
+    char            *key;
+    char            *value;
+}   t_env_var;
+
