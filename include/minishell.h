f485b57c4ec3da94197b6f7910fcdc5833431d88
yetkin <glserozkan@gmail.com>
2023-07-26 11:46:07 +0200

    header updated

--- include/minishell.h
+++ include/minishell.h
@@ -4,46 +4,58 @@
 
 # include "../libft/libft.h"
 
-typedef enum s_tokens
+extern int glob_exit_status; //with extern, we declare glob_exit_status without defining
+//we ll define it later in the program
+
+typedef enum s_token_type
 {
     PIPE = 1,
+    LITERAL,
     GREAT,
     GREAT_GREAT,
     LESS,
     LESS_LESS,
-}   t_tokens;
+}   t_token_type;
 
 typedef struct s_token
 {
     char            *content;
-    int             token_id;
+    t_token_type    type;
     struct s_token  *next;
 }   t_token;
 
 
-typedef struct s_tools
+typedef struct s_redirection
 {
-    char **env;
-    struct s_env_var *env_var;
-    struct s_command *command;
-}t_tool;
-
+    char            *file_name; //if it s <<, heredoc, call heredoc f()
+    t_token_type    type;
+    struct s_redirection *next;
+}t_redirection;
 
 typedef struct s_command
 {
     char **args;
-
-
-
-
+    t_redirection *redirection;
+    struct s_command *next;
 }t_command;
 
-BUILTINS
 
-typedef struct s_env_var
+typedef struct s_tools
 {
-    struct s_env    *next;
+    char *input;
+    char **env;
+    struct s_env_node *env_list;
+    t_command *command_list;
+   // t_token *history;
+    int number_of_pipes;
+
+}t_tools;
+
+
+typedef struct s_env_node
+{
+    struct s_env_node    *next;
     char            *key;
     char            *value;
-}   t_env_var;
+}   t_env_node;
 
