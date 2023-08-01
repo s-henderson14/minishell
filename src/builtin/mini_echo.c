cb254031f10b6455a90aef561dc814f21fdc21cf
yetkin <glserozkan@gmail.com>
2023-08-01 10:38:00 +0200

    echo is done

--- src/builtin/mini_echo.c
+++ src/builtin/mini_echo.c
@@ -6,17 +6,33 @@ void print_home(char *home, t_tools *tools)
 	char *home_value;
 
 	env_list = tools->env_list;
-	home_value = get_value_from_env_node(home, env_list);
+	home_value = get_value_from_env_node(home, env_list); //NOT NECESSARY AT ALL!!!
 	printf("%s");
+}
+
+/*
+**	Prints strings of char **args array
+**	param i : starting index of the strings array for printing
+*/
+void print_arguments(int i, t_command *command)
+{
+	char **args;
+
+	args = command->args;
+	while (args[i] != NULL)
+	{
+		if (ft_strsame(args[i], "~") == 1)
+			print_home("HOME", tools);
+		else
+			printf("%s", args[i]);
+		if (args[i + 1] != NULL)
+			printf(" ");
+		i++;
+	}
 }
 
 int mini_echo(t_tools *tools, t_command *command)
 {
-	// if (command->args[1] == NULL)
-	// {
-	// 	printf("\n");
-	// 	return (glob_exit_status);
-	// }
 	char **args;
 	int i;
 	int print_newline;
@@ -24,12 +40,21 @@ int mini_echo(t_tools *tools, t_command *command)
 	args = command->args;
 	i = 1
 	print_newline = 1;
-	while (args[i][0] == '-' && args[i][1] == 'n')
+	while (args[i] != NULL && args[i][0] == '-' && args[i][1] == 'n')
 	{
-		while (args[i][j] == 'n') // -nnnnnn
+		j = 1;
+		if (args[i][j] != '\0' && args[i][j] != 'n')
+			break ;
+		while (args[i][j] != '\0' && args[i][j] == 'n') // -nnnnnn
 			j++;
-		if (ft_strsame(args[i + 1][0], "~") == 1)
-			print_home("HOME", tools);
-		
+		if (args[i][j] == '\0')
+		{
+			print_newline = 0;
+			break ;
+		}
+		i++;
 	}
+	print_arguments(i, command);
+	if (print_newline == 1)
+		printf("\n");
 }
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -5,7 +5,7 @@ int error_exit(char *s)
 	glob_exit_status = 1;
 	if (s == NULL)
 		return (glob_exit_status);
-	ft_putstr_fd(s, STDOUT_ERROR);
+	ft_putstr_fd(s, STDERR_FILENO);
 	write(fd, "\n", 1);
 	return (glob_exit_status);
 }
