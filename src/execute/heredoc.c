bfbbcb0071408e3d0a0ca71483affb7391c1cab3
yetkin <glserozkan@gmail.com>
2023-08-07 16:40:19 +0200

    heredoc continue

--- src/execute/heredoc.c
+++ src/execute/heredoc.c
@@ -3,7 +3,14 @@
 int here_document(t_redirection *redirection)
 {
 	char *line;
+	char *delimiter;
 	int fd[2];
 
-	
+	delimiter = redirection->file_name;
+	while (1)
+	{
+		line = readline("> ");
+		if (ft_strsame(line, delimiter) == 1)
+			break ;
+	}
 }
