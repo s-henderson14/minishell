227ef9059de9e603958b34327c0b8026c92f213a
s-henderson14 <106280033+s-henderson14@users.noreply.github.com>
2023-09-08 14:41:04 +0200

    Add files via upload

--- src/parser/create_cmds.c
+++ src/parser/create_cmds.c
@@ -1,47 +1,5 @@
 #include "../include/minishell.h"
 
-void	add_cmd_front(t_command **cmd_lst, t_command *new_cmd)
-{
-	new_cmd->next = *cmd_lst;
-	*cmd_lst = new_cmd;
-}
-
-void	add_cmd_back(t_command **cmd_lst, t_command *new_cmd)
-{
-	t_command	*tmp;
-
-	tmp = *cmd_lst;
-	if (*cmd_lst == NULL)
-	{	
-		add_cmd_front(cmd_lst, new_cmd);
-		return ;
-	}
-  	while (tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = new_cmd;
-}
-
-int	word_counter(const char *s, char c)
-{
-	int	count;
-	int	i;
-
-	i = 0;
-	count = 0;
-	while (s[i])
-	{
-		while (s[i] && s[i] == c)
-			i++;
-		while (s[i] && s[i] != c)
-		{	
-			i++;
-			if (s[i] == c || s[i] == '\0')
-				count++;
-		}
-	}
-	return (count);
-}
-
 t_command	**create_simple_cmd(t_token **tkn_list, t_tool *shell)
 {
 	t_command	**cmd_list;
